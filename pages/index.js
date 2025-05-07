9b2d5ba9d11fd09110d32632252fce3db9485544
Varshith3 <varshithmuppinidi30@gmail.com>
2025-05-07 23:03:59 +0530

    Redirect logic

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { supabase } from './Client';
 
 export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push('/signup'); // Redirect to the Signup page
+    const checkUser = async () => {
+      const { data: { user } } = await supabase.auth.getUser();
+
+      if (user) {
+        router.push('/dashboard');
+      } else {
+        router.push('/signup');
+      }
+    };
+
+    checkUser();
   }, []);
 
-  return null; // Render nothing while redirecting
+  return null;
 }
