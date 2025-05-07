5a4e62c3dbf5d8e27fc9d98c4985d2ee027b5e6e
Varshith3 <varshithmuppinidi30@gmail.com>
2025-05-07 21:47:37 +0530

    Initial commit

--- pages/Client.js
+++ pages/Client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://yxqdswberwmvuybqclwi.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4cWRzd2JlcndtdnV5YnFjbHdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MjQ0MDQsImV4cCI6MjA2MjEwMDQwNH0.pGSJP60z2Fi1Em5Qp2vC9ciKxsD1IB9GoI49tDfHMRk';
+const supabaseAnonKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4cWRzd2JlcndtdnV5YnFjbHdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MjQ0MDQsImV4cCI6MjA2MjEwMDQwNH0.pGSJP60z2Fi1Em5Qp2vC9ciKxsD1IB9GoI49tDfHMRk';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
